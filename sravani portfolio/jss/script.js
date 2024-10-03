5200d4cc1f768234cc0bbefa8ec0810e2801497c
sravanikondapelly <sravanikondapelli@gmail.com>
2024-10-03 21:57:53 +0530

    Add files via upload

---
+++ sravani portfolio/jss/script.js
@@ -0,0 +1,8 @@
+
+/*=================================== typing animation ==================================*/
+var typed = new Typed(".typing",{
+    strings:["","student"],
+    typespeed:100,
+    Bachspeed:60,
+    loop:true
+})
---
+++ sravani portfolio/jss/style-switcher.js
@@ -0,0 +1,25 @@
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("cilck", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+window.addEventListener("scroll",() => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+const alternative = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach(style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.removeAttribute("disabled","true");
+        }
+    }
+}
